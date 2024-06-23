304a67563af54b8cb9270717951fede8ecbd7191
Christian Stangerup <christian@westerkam.org>
2024-06-23 12:03:11 +0200

    Working on config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,8 +3,8 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Stangerup Cloud Documentation',
+  tagline: 'Get your cloud setup in order',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
